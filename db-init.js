b1c158b5e2facffce4951ae45353c1229fb1f610
Ruslan Karachun <free.changer@gmail.com>
2016-02-26 13:31:29 +0300

    Create db script

---
+++ db-init.js
@@ -0,0 +1,36 @@
+var fs = require("fs")
+  , db_file = "db.sq3"
+  , exists = fs.existsSync(db_file);
+
+if (!exists) {
+  console.log("Creatind DB file.")
+  fs.openSync(db_file, "w");
+}
+
+var sqlite3 = require("sqlite3").verbose()
+  , db = new sqlite3.Database(db_file);
+
+db.serialize(function () {
+  if (!exists) {
+    db.run(
+      "CREATE TABLE urls ("
+        + "id INTEGER PRIMARY KEY,"
+        + "counter INT,"
+        + "path CHAR,"
+        + "long_url CHAR(256)"
+        + ")"
+    );
+  }
+
+  var stmt = db.prepare("INSERT INTO urls VALUES (?, ?, ?, ?)");
+  var rnd;
+  for (var i = 0; i < 10; i++) {
+    rnd = 500 - i;
+    stmt.run([i, 0, rnd, "http://google.com"]);
+  } 
+
+  stmt.finalize();
+
+});
+
+db.close();
