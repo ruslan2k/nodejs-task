3ae9fdcbf9f7730aca8163d4ab32135b7172578e
Ruslan Karachun <free.changer@gmail.com>
2016-02-27 19:13:03 -0500

    Add convert funcs, add model

---
+++ libfuncs.js
@@ -0,0 +1,57 @@
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var possible_length = possible.length;
+var DEF_L = 6;
+
+module.exports = {
+  getRandomString: function (l)
+  {
+    var text = "";
+    l = typeof l !== 'undefined' ? l : DEF_L;
+  
+    for (var i = 0; i < l; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible_length));
+    }
+  
+    return text;
+  },
+  
+  genUid: function (l)
+  {
+    var int_uid = 0;
+    var str_uid = "";
+    l = typeof l !== 'undefined' ? l : DEF_L;
+
+    for (var i = 0; i < l; i++) {
+      var r = Math.floor(Math.random() * possible_length);
+      var c = possible.charAt(r);
+      str_uid += c;
+      int_uid = int_uid * 100 + c.charCodeAt() - 64;
+    }
+  
+    return {"int_uid": int_uid, "str_uid": str_uid};
+  },
+
+  // intUidtoStrUid: function (int_uid)
+  // {
+  //   var str_uid = "";
+  //   for (; int_uid > 1;) {
+  //     var i = int_uid % 100;
+  //     var int_uid = int_uid / 100;
+  //     str_uid = possible.charAt(i) + str_uid; 
+  //   }
+
+  //   return str_uid;
+  // },
+
+  strUidtoIntUid: function (str_uid)
+  {
+    var int_uid = 0;
+    var l = str_uid.length;
+
+    for (var i = 0; i < l; i++) {
+      int_uid = int_uid * 100 + str_uid.charAt(i).charCodeAt() - 64;
+    }
+
+    return int_uid;
+  }
+};
---
+++ short_urls.js
@@ -0,0 +1,17 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database("db.sq3");
+
+
+module.exports = {
+  get: function (id) {
+    int_id = stringToIntId(id);
+
+
+  },
+
+  insert: function (data) {
+    //    
+  }
+
+
+}
