6d4727388ff1aba1e58038debf197e034fe51018
Ruslan Karachun <free.changer@gmail.com>
2016-02-29 10:34:02 +0300

    Add route without model

--- routes/shorturls.js
+++ routes/shorturls.js
@@ -1,31 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var funcs = require('../libs/funcs');
-var ShortUrl = require('../models/shorturl'); 
-
-function _handleModelRes (res, successStatus) {
-  return function (err, playload) {
-    if (err) {
-      console.error(err);
-      res.status(err.code).send(err.message);
-      return;
-    }
-    if (successStatus) {
-      res.status(successStatus);
-    }
-    console.log("playload:" + playload);
-    return playload;
-  };
-}
+var db = require('../db');
 
 /* Get short urls list */
 router.get('/', function (req, res) {
-  //res.render('urls', {urls: [1, 2, 3]});
-  //console.log(ShortUrl.getAll(next));
-  //next();
-  var urls = ShortUrl.getAll(_handleModelRes(res));
-  console.log("urls:" + urls);
-  res.render('urls', {"urls": urls});
+  db.all("SELECT * FROM urls", function (err, rows) {
+    res.json({"rows": rows});
+  });
 });
 
 router.post('/', function (req, res) {
